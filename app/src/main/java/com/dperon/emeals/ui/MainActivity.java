073ef2566b76af8c4cd5b56f14bedd16cb097bbd
Damian Peron <Damian.Peron@endava.com>
2020-04-17 19:44:53 -0300

    Initial commit

---
+++ app/src/main/java/com/dperon/emeals/ui/MainActivity.java
@@ -0,0 +1,21 @@
+package com.dperon.emeals.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dperon.emeals.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+}
